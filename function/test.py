09767327ea198abc841f2fc55b7f28606a204429
Sean Huggins <seanhugg@gmail.com>
2022-11-17 16:06:55 -0800

    Update test.py

--- function/test.py
+++ function/test.py
@@ -3,10 +3,16 @@ import unittest
 def uppercase(text):
   return text.upper()
 
+def lowercase(text):
+  return text.lower()
+
 class Test(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual(uppercase("hello"), "HELLO")
+    def test_uppercase(self):
+        self.assertEqual(uppercase("hEllO"), "HELLO")
+        
+    def test_lowercase(self):
+        self.assertEqual(lowercase("WoRLD"), "world")
 
 if __name__ == '__main__':
     unittest.main()
